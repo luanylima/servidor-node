35ae8b3876fdd8726722f651bdabb2bc43d18269
luanyy <luany.lima@escola.pr.gov.br>
2026-09-01 11:37:36 -0300

    COMEÇO

---
+++ server.js
@@ -0,0 +1,18 @@
+// Importar o modulo nativo "http" do node.js//
+const http = require('http');
+
+//define o endereço(localhost- minha maquina local) e a porta onde o servidor vai escutar
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//cria servidor web
+const server = http.createServer( (req, res)=>{
+
+   // define o status http como 200 (ok) e o tipo de conteudo como texto plano 
+
+});
+
+
+const name= (a,b) => {
+
+}
